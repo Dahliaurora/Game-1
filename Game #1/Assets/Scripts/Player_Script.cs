de7c1f43b2fbf0bf34dff3274c86de341c64e4b8
Dahliaurora <aurorajewel2007@gmail.com>
2025-08-06 17:04:32 -0700

    ItBegins
    ...rock

---
+++ Game #1/Assets/Scripts/Player_Script.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player_Script
+{
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(direction*_playerSpeed*Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInpt, verticalinput, 0);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.96f, 0), 0);
+    }
+}
